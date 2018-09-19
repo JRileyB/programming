1c8ab86c45c19d3319e63d4bc322a4cc5a40c1ba
Riley Bateman <j.riley.bateman@gmail.com>
2018-09-18 21:58:35 -0600

    Beginnings of header bar
    adding the header bar with the logo

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react'
-
-const message = 'This is my super cool message'
+import Header from './components/header'
+import './styles.css'
 
 export default class App extends React.Component<any, any> {
  render() {
      return (
-         <div>
-         <div>{message}</div>
-        <img width={400} src="http://cdn.kickvick.com/wp-content/uploads/2015/09/cutest-bunny-rabbits.jpg" alt="some bunny"/>
+         <div className={'main'}>
+             <Header />
         </div> 
     )
  }
---
+++ client/src/components/header.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import './header.css'
+
+
+const Header = () => {
+return (
+    <header className ={'header'}>
+        <img src="/assets/img/logo.png" alt="Riley & Kirsten Bateman Logo"/>
+        //look into flexbox for the location of things
+        //Watch PS course on flexbox
+    </header>
+)
+
+}
+
+export default Header
